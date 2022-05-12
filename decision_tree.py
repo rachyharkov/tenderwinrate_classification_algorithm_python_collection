f64d04d1b255de72baeb2d53549267c3065f3f87
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-05-12 12:07:34 +0700

    Logistic_regression done

--- decision_tree.py
+++ decision_tree.py
@@ -7,42 +7,66 @@ from sklearn.model_selection import train_test_split # Import train_test_split f
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 
+cols = ['harga', 'partner', 'competitor', 'winrate']
 
+dataset = pd.read_csv("training_data.csv", sep=";",header=None, names=cols)
+
+
+x = dataset.iloc[:, :3].values
+y = dataset.iloc[:, -1].values
+
+print(x)
+print(y)
+
+winlosscategory = np.unique(y)
+# get the unique values of categorical data
+rangeUnique = np.unique(x[:, 0])
+print(rangeUnique)
+# then change x based on the unique values
+for i in range(len(x)):
+    for j in range(len(x[i])):
+        # detect if the value is in the range of the unique values
+        if x[i][j] in rangeUnique:
+            x[i][j] = rangeUnique.tolist().index(x[i][j])
+
+
+# convert y to binary
+y = np.where(y == "Win", 1, 0)
+
+print(x)
+print(y)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+print(len(x_train))
+print(len(x_test))
+print(len(y_train))
+print(len(y_test))
+
+
+# feature scaling
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+print(x_train)
+
+clf = DecisionTreeClassifier()
+
+# Train Decision Tree Classifer
+clf = clf.fit(x_train,y_train)
+
+#Predict the response for test dataset
+# y_pred = clf.predict(x_test)
+y_pred = clf.predict(x_test)
 
 def run_model(new_x):
-    print('Processing data...')
-
-    dataset = pd.read_csv("training_data.csv", header=None, sep=';')
-
-    x = dataset.iloc[:, :3].values
-    y = dataset.iloc[:, -1].values
-
-    # append new data to the dataset
-
-    print(x)
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20) # 80% training and 20% test
-
-    clf = DecisionTreeClassifier()
-
-    # Train Decision Tree Classifer
-    clf = clf.fit(x_train,y_train)
-
-    #Predict the response for test dataset
-    # y_pred = clf.predict(x_test)
 
     
-    y_pred = clf.predict(x_test)
     # predict accuracy
-    winLossEvaluation = winlossunique[y_pred[0]]
-
-    # accuracy = np.mean(cross_val_score(clf, x_test, y_pred, scoring='accuracy')) * 100
-    # string = "Input Test for : "
-
     y_pred = clf.predict(new_x)
-    # for x in x_test:
-    #     string += str(x) + ' (' + str(rangeUnique[x]) + ')'
-    print(metrics.classification_report(y_test, y_pred))
+    winLossEvaluation = winlosscategory[y_pred[0]]
 
     # print(string)
     # print("Accuracy: {}%".format(accuracy))
